69648fd37d6de06e6fab243ec9328e254c2e5ffd
farhat-jojma <farhat.jojma@gmail.com>
2025-08-26 15:05:24 +0100

    chore: config cloudeflare

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: "export",
   images: {
     // autorise les images distantes depuis picsum.photos
     domains: ["picsum.photos"],

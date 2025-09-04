4c6ae3b33085f757141fde27319f585fb2a14429
farhat-jojma <farhat.jojma@gmail.com>
2025-09-04 11:06:06 +0100

    chore: fix cloudflare deployment error 3

--- src/app/page.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { redirect } from "next/navigation";
-
-export default async function RootPage() {
-  // Redirect root path to default locale, e.g., /en
-  redirect("/en");
-}

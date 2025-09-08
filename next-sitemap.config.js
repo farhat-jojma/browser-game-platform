b544586f100a5e7458b51985e5a6af8e76522a27
farhat-jojma <farhat.jojma@gmail.com>
2025-09-08 14:48:38 +0100

    chore: create sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.gamesonlinegratis.com', // <-- replace with your domain (no trailing slash)
+  generateRobotsTxt: true, // generates robots.txt
+  sitemapSize: 5000, // split if you have many URLs
+};
